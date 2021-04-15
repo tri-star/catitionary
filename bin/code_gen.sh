215f780bb7b76a3e26b8eaa2a3262790d75a5fee
tristar <tristar.2nd@gmail.com>
2021-04-16 04:00:27 +0900

    Open API Generator関連のコードの追加。
    一旦は対応を見送る

--- bin/code_gen.sh
+++
@@ -1,6 +0,0 @@
-sudo rm -rf mock/php
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli list
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
-    -i /local/swagger/swagger.yml \
-    -g php-lumen \
-    -o /local/mock/php
