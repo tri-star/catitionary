91ec3d060d4e27209b1bfe6635276917f5334b57
tristar <tristar.2nd@gmail.com>
2021-05-16 10:23:12 +0900

    Tailwindの追加

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./javascript/html/index.html', './javascript/**/*.{vue,js}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
