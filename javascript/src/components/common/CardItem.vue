4d85268a30754089cbd6c3c1c6b0c16e4e80e91d
tristar <tristar.2nd@gmail.com>
2021-05-23 17:31:16 +0900

    初期画面の作成中

---
+++ javascript/src/components/common/CardItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-secondary-300 p-5 rounded-lg"><slot /></div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
--- javascript/src/pages/Home.vue
+++ javascript/src/pages/Home.vue
@@ -1,15 +1,34 @@
 <template>
   <DefaultLayout>
-    <div>コンテンツ2</div>
+    <div>
+      <CardItem>
+        <div class="flex">
+          <span class="self">種類</span>
+          <select>
+            <option>テスト</option>
+          </select>
+        </div>
+      </CardItem>
+      <CardItem>
+        <div class="flex">
+          <span class="self">特徴</span>
+          <select>
+            <option>テスト</option>
+          </select>
+        </div>
+      </CardItem>
+    </div>
   </DefaultLayout>
 </template>
 
 <script>
 import DefaultLayout from '@/layouts/DefaultLayout'
+import CardItem from '@/components/common/CardItem'
 
 export default {
   components: {
     DefaultLayout,
+    CardItem,
   },
   setup() {},
 }
