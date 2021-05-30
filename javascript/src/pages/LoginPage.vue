4920820b070d48df6d502ab867c0859b335ac401
tristar <tristar.2nd@gmail.com>
2021-05-30 09:53:57 +0900

    ログイン画面： フォームのレイアウト調整

--- javascript/src/pages/LoginPage.vue
+++ javascript/src/pages/LoginPage.vue
@@ -3,35 +3,31 @@
     <div class="flex flex-col items-center">
       <div class="container w-11/12 pt-3">
         <div class="w-6/12 mx-auto my-20">
-          <h1 class="text-6xl w-full text-center mb-10">Example App</h1>
-          <div class="form">
-            <div class="form-row">
-              <div class="form-header w-3/12">ログインID</div>
-              <div class="form-col w-9/12">
+          <h1 class="text-6xl w-full text-center mb-10">Catitionary</h1>
+          <FormRowList class="items-center">
+            <LabeledFormRow label="ログインID" label-width="w-20">
+              <template slot="form">
                 <TextInput v-model="state.form.loginId" class="w-full" />
-              </div>
-            </div>
-            <div class="form-row">
-              <div class="form-header w-3/12">パスワード</div>
-              <div class="form-col w-9/12">
+              </template>
+            </LabeledFormRow>
+            <LabeledFormRow label="パスワード" label-width="w-20">
+              <template slot="form">
                 <TextInput
                   v-model="state.form.password"
                   :type="'password'"
                   class="w-full"
                 />
-              </div>
-            </div>
-            <div class="form-row">
-              <div class="form-col mx-auto">
-                <SimpleButton
-                  :title="'ログイン'"
-                  class="mr-3"
-                  :disabled="state.isError"
-                  @onclick="onLoginClicked"
-                />
-              </div>
-            </div>
-          </div>
+              </template>
+            </LabeledFormRow>
+            <FormRow>
+              <SimpleButton
+                :title="'ログイン'"
+                class="mr-3"
+                :disabled="state.isError"
+                @onclick="onLoginClicked"
+              />
+            </FormRow>
+          </FormRowList>
         </div>
       </div>
     </div>
@@ -43,12 +39,18 @@ import { useValidator } from '@/hooks/useValidator'
 import { constraints } from '@/lib/validator/constraints'
 import { RuleCollection } from '@/lib/validator/Rule'
 import { defineComponent, inject, reactive, watch } from '@vue/composition-api'
+import FormRowList from '@/components/common/form/FormRowList'
+import FormRow from '@/components/common/form/FormRow'
+import LabeledFormRow from '@/components/common/form/LabeledFormRow'
 import SimpleButton from '@/components/common/button/SimpleButton'
 import TextInput from '@/components/common/TextInput'
 import { useRouter } from '@/hooks/useRouter'
 
 export default defineComponent({
   components: {
+    FormRow,
+    FormRowList,
+    LabeledFormRow,
     SimpleButton,
     TextInput,
   },
