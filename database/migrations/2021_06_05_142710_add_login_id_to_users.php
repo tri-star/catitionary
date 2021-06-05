cfef73a6dd1ee00783b75374d8d858f6ddf48c9a
tristar <tristar.2nd@gmail.com>
2021-06-06 01:17:18 +0900

    ログインIDカラムの追加

---
+++ database/migrations/2021_06_05_142710_add_login_id_to_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddLoginIdToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('login_id', 30)->after('name');
+            $table->index(['login_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['login_id']);
+        });
+    }
+}
